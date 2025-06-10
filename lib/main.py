eaa955accdca9553a1e6ab8cdad579f5fd98422c
sjc230 <70530110+sjc230@users.noreply.github.com>
2025-06-10 15:11:46 -0400

    correction

--- lib/main.py
+++ lib/main.py
@@ -439,7 +439,7 @@ mean_shift_button.grid(row=2,column=1,padx=10, pady=10)
 kmeans_button = customtkinter.CTkButton(tab_3, text="k means", command=open_decision_tree_window)
 kmeans_button.grid(row=0,column=2,padx=10, pady=10)
 
-bi_kmeans_button = customtkinter.CTkButton(tab_3, text="bisecting k means", command=open_decision_tree_window)
+bi_kmeans_button = customtkinter.CTkButton(tab_3, text="bisecting k means", command=open_kmeans_window)
 bi_kmeans_button.grid(row=1,column=2,padx=10, pady=10)
 
 mini_kmeans_button = customtkinter.CTkButton(tab_3, text="mini-batch k means", command=open_decision_tree_window)
