6ea277562f1710382a9bc52b6edd59329d1a6b2f
sjc230 <70530110+sjc230@users.noreply.github.com>
2025-06-25 15:42:08 -0400

    page corrections

--- Homepage.py
+++ Homepage.py
@@ -8,6 +8,12 @@ st.set_page_config(
     page_title="Sensor AI Framework",
 )
 
-st.title("Home Page")
-st.write("Welcome to the Sensor AI Framework")
+st.title("Sensor AI Framework")
+st.write("Welcome to the Center for Cyber-Physical Systems' artificial intelligence framework for sensor data")
 st.sidebar.success("Select a page above")
+
+if "data_file" not in st.session_state:
+    st.session_state["data_file"] = ""
+
+if "label_file" not in st.session_state:
+    st.session_state["label_file"] = ""
