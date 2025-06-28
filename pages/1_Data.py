1f61423f3a74365fe6a6639dd25bfbeb403ba49d
sjc230 <70530110+sjc230@users.noreply.github.com>
2025-06-28 16:03:59 -0400

    waveform data generator added

--- pages/1_Data.py
+++ pages/1_Data.py
@@ -14,7 +14,7 @@ other_folder_path = parent_dir.parent / "lib"
 # Add the other folder to sys.path so Python can find the module
 sys.path.append(str(other_folder_path))
 # Now you can import from file2.py
-from utils import data_setup
+from utils import data_setup, parse_text_entry, save_numpy_array, generate_class_data, generate_anomaly_data, generate_regression_data
 
 st.title("Data")
 
@@ -41,8 +41,10 @@ if st.button("Clear All Data"):
 
 
 option = st.selectbox('What data would you like to use?',
-    ('.csv or .npy file', 'influx download', 'generate waveforms', 'generate scg signals'))
+    ('.csv or .npy file', 'generate waveforms', 'generate scg signals'))
 
+
+# .CSV or .NPY File Load Options
 if option == '.csv or .npy file':
     header_check = st.checkbox("Does your data/label file(s) have headers? Check for yes",key="header check")
 
@@ -62,81 +64,101 @@ if option == '.csv or .npy file':
         if split_value != 0.00:
             split = split_value
 
-if option == '.csv or .npy file':
-    ready = st.checkbox("Select when ready to choose your file(s)",key = "ready to load")
-
-    if ready == True:
-        uploaded_file = st.file_uploader("Choose a CSV or NPY data file", type=["csv","npy"],key="data loader")
-
-        if sep_labels == True:
-            uploaded_labels = st.file_uploader("Choose a CSV or NPY label file", type=["csv","npy"],key="label loader")
-            if uploaded_labels is not None:
-                root, extension = os.path.splitext(uploaded_labels.name)
-                if extension.lower() == ".npy":
-                    labels = np.load(uploaded_labels)                
-                else:
-                    if header_check == False:
-                        df = pd.read_csv(uploaded_labels,header=None)
-                    else:
-                        df = pd.read_csv(uploaded_labels)
-                    labels = df.to_numpy
-        else:
-            labels = None
-    
-
-        if uploaded_file is not None:
-            root, extension = os.path.splitext(uploaded_file.name)
-            if extension.lower() == ".npy":
-                data = np.load(uploaded_file)
-                df = pd.DataFrame(data)                
-            else:
-                if header_check == False:
-                    df = pd.read_csv(uploaded_file,header=None)
-                else:
-                    df = pd.read_csv(uploaded_file)
-                data = df.to_numpy 
-
-            st.write("File uploaded ...")
-
-            st.subheader("Data Preview")
-            st.write(df.head())
-
-            st.subheader("Data Summary")
-            st.write(df.describe())    
-        
-        else:
-            st.write("Waiting on file upload")
-    
-        if (uploaded_file is not None) and (sep_labels == True):
-            X_train, y_train, X_test, y_test = data_setup(label_bool=label_check,
-                                                          sep_data_bool=sep_labels,
-                                                          split_bool=split_check,
-                                                          data_file=data,
-                                                          split_value=split,
-                                                          label_file=labels)
-            st.session_state["X_train"] = X_train
-            st.session_state["y_train"] = y_train
-            st.session_state["X_test"] = X_test
-            st.session_state["y_test"] = y_test
-        elif (uploaded_file is not None) and (sep_labels == False):
-            X_train, y_train, X_test, y_test = data_setup(label_bool=label_check,
-                                                          sep_data_bool=sep_labels,
-                                                          split_bool=split_check,
-                                                          data_file=data,
-                                                          split_value=split,
-                                                          label_file=labels)
-            st.session_state["X_train"] = X_train
-            st.session_state["y_train"] = y_train
-            st.session_state["X_test"] = X_test
-            st.session_state["y_test"] = y_test
-
-elif option == 'influx download':
-    st.write("influx download selected")
-
+# Generate Wavefor Data Options
 elif option == 'generate waveforms':
-    st.write("generate waveforms selected")
+    wav_cat = st.text_input("Category: classification, detection, or regression", value='classification', key="category")
+    wav_amp = st.number_input("Amplitude: float, 0.00 for None", value=0.00, step=0.01, format="%.2f" key="wave amp")
+    wav_freq = st.number_input("Frequency: float, 0.00 for None", value=0.00, step=0.01, format="%.2f", key="wave freq")
+    wav_noise = st.checkbox("Check to add Noise", key="wave noise box")
+    wav_num = st.number_input("Wave Number: integer", value='10', min_value='int', key="wave num")
+    wav_label = st.text_input("Labels: amplitude or frequency", value='frequency', key="wave labels")
+
+   
+    if st.button("Generate the Wave Data"):
+        if wav_amp == 0.0:
+            wav_amp = None
+        if wav_freq == 0.0:
+            wav_freq == None
+        
+        if wav_cat == 'classification':
+            data, labels = generate_class_data(amplitude=wav_amp,frequency=wav_freq,noise=wav_noise,wav_number=wav_num,show=True)
+        elif wav_cat == 'detection':
+            data, labels = generate_anomaly_data(amplitude=wav_amp,frequency=wav_freq,noise=wav_noise,wave_number=wav_num,show=True)
+        else:
+            data, labels = generate_regression_data(amplitude=wav_amp,frequency=wav_freq,noise=wav_noise,wave_number=wave_num,label_type=wav_label,show=True)
+
+        labels = labels.reshape(-1, 1)
+        data_file = np.concatenate((data,labels),axis=1)
+
+        save_numpy_array(data_file)
 
 elif option == 'generate scg signals':
     st.write("generate scg signals selected")
 
+
+ready = st.checkbox("Select when ready to choose your file(s)",key = "ready to load")
+
+if ready == True:
+    uploaded_file = st.file_uploader("Choose a CSV or NPY data file", type=["csv","npy"],key="data loader")
+
+    if sep_labels == True:
+        uploaded_labels = st.file_uploader("Choose a CSV or NPY label file", type=["csv","npy"],key="label loader")
+        if uploaded_labels is not None:
+            root, extension = os.path.splitext(uploaded_labels.name)
+            if extension.lower() == ".npy":
+                labels = np.load(uploaded_labels)                
+            else:
+                if header_check == False:
+                    df = pd.read_csv(uploaded_labels,header=None)
+                else:
+                    df = pd.read_csv(uploaded_labels)
+                labels = df.to_numpy
+    else:
+        labels = None
+
+
+    if uploaded_file is not None:
+        root, extension = os.path.splitext(uploaded_file.name)
+        if extension.lower() == ".npy":
+            data = np.load(uploaded_file)
+            df = pd.DataFrame(data)                
+        else:
+            if header_check == False:
+                df = pd.read_csv(uploaded_file,header=None)
+            else:
+                df = pd.read_csv(uploaded_file)
+            data = df.to_numpy 
+
+        st.write("File uploaded ...")
+
+        st.subheader("Data Preview")
+        st.write(df.head())
+
+        st.subheader("Data Summary")
+        st.write(df.describe())    
     
+    else:
+        st.write("Waiting on file upload")
+
+    if (uploaded_file is not None) and (sep_labels == True):
+        X_train, y_train, X_test, y_test = data_setup(label_bool=label_check,
+                                                        sep_data_bool=sep_labels,
+                                                        split_bool=split_check,
+                                                        data_file=data,
+                                                        split_value=split,
+                                                        label_file=labels)
+        st.session_state["X_train"] = X_train
+        st.session_state["y_train"] = y_train
+        st.session_state["X_test"] = X_test
+        st.session_state["y_test"] = y_test
+    elif (uploaded_file is not None) and (sep_labels == False):
+        X_train, y_train, X_test, y_test = data_setup(label_bool=label_check,
+                                                        sep_data_bool=sep_labels,
+                                                        split_bool=split_check,
+                                                        data_file=data,
+                                                        split_value=split,
+                                                        label_file=labels)
+        st.session_state["X_train"] = X_train
+        st.session_state["y_train"] = y_train
+        st.session_state["X_test"] = X_test
+        st.session_state["y_test"] = y_test
---
+++ pages/8_Historical_Data_Connection.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import yaml
+import numpy as np
+import os
+import sys
+import threading
+import tempfile
+from pathlib import Path
+
+# Get the path of the current file (file1.py)
+current_file_path = Path(__file__).resolve()
+# Get the parent directory (folder1)
+parent_dir = current_file_path.parent
+# Get the path to the other folder (folder2)
+other_folder_path = parent_dir.parent / "lib"
+# Add the other folder to sys.path so Python can find the module
+sys.path.append(str(other_folder_path))
+# Now you can import from file2.py
+
+from utils import run_script, data_loader_confg, convert_list_to_string
+from dsp import *
+
+
+current_dir = Path.cwd() # assumes your working directory is in "SensorwebAiFramework"
+
+temp_dir = tempfile.mkdtemp()
+
+if 'device_name' not in st.session_state:
+    st.session_state['device_name'] = ""
+
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = ""
+
+if 'conn_script' not in st.session_state:
+    st.session_state['conn_script'] = ""
+
+if 'stop_event' not in st.session_state:
+    st.session_state['stop_event'] = ""
+
+st.title("Historical Data Connection")
+
+
+# Method to connect to db
+def connect_model_to_db(device_name,model_name):
+    print("Connecting - model: ",model_name,"To database: ",device_name)
+
+    st.session_state["stop_event"] = threading.Event()
+    script_path = current_dir / "mqtt/main_ai_influx.py"
+    arguments = [device_name, model_name]
+
+    conn_script = threading.Thread(target=run_script, args=(script_path, arguments, st.session_state["stop_event"]))    
+    conn_script.start()
+    st.session_state['conn_script'] = conn_script
+
+# Method to disconnect from db
+def disconnect_model_from_db():
+    st.session_state["stop_event"].set()
+    st.session_state['conn_script'].join()
+    st.session_state['device_name'] = ""
+    st.session_state['model_name'] = ""
+    #st.session_state['database yaml loader'] = ""
+    #st.session_state['model yaml loader'] = ""
+    st.session_state["stop_event"] = ''
+    st.session_state['conn_script'] = ''
+    print("Thread terminated.")
+
+db_yaml = st.file_uploader("Choose a yaml file for a database", type="yaml",key="database yaml loader")
+if db_yaml is not None:
+    root, extension = os.path.splitext(db_yaml.name)
+    if extension.lower() == ".yaml":
+        db_path = os.path.join(temp_dir, db_yaml.name)
+        with open(db_path, "wb") as f:
+            f.write(db_yaml.getvalue())
+        st.session_state['device_name'] = db_path
+                  
+    else:
+        st.write("You have selected an incorrect file type")
+
+
+#save_data_check = st.checkbox("Select if you want to save downloaded data to a .npy file", key = "hist download save")
+
+h_model_yaml = st.file_uploader("Choose a yaml file for your model", type="yaml",key="hist model yaml loader")
+if h_model_yaml is not None:
+    root, extension = os.path.splitext(h_model_yaml.name)
+    if extension.lower() == ".yaml":
+        h_model_path = os.path.join(temp_dir, h_model_yaml.name)
+        with open(h_model_path, "wb") as f:
+            f.write(h_model_yaml.getvalue())
+        st.session_state['model_name'] = h_model_path  
+                  
+    else:
+        st.write("You have selected an incorrect file type")
+
+if st.session_state['model_name'] != "" and st.session_state['device_name'] != "":
+    if st.button("connect mode to device"):
+        connect_model_to_db(device_name=st.session_state['device_name'],model_name=st.session_state['model_name'])
+    if st.button("disconnect model from device"):
+        disconnect_model_from_db()
