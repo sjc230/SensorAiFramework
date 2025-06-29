d50cafabcfb4b5a5907e291dfe4c266e6b912f6c
sjc230 <70530110+sjc230@users.noreply.github.com>
2025-06-29 16:03:36 -0400

    extra trees added

--- pages/3_Classification.py
+++ pages/3_Classification.py
@@ -43,16 +43,19 @@ model_tuple = ('decision tree', 'extra trees', 'random forest', 'gradien boostin
 
 model_box = st.selectbox('**Select the model(s) you like to use.**', model_tuple)
 
+########################################################
+# Model Variable Entries
+########################################################
+
+# Decision Tree
 if model_box == 'decision tree':
-    dec_crit = st.text_input("Criterion: gini, entropy, or log_los", value='gini', key="dec crit")
+    dec_crit = st.text_input("Criterion: gini, entropy, or log_loss", value='gini', key="dec crit")
     dec_split = st.text_input("Splitter: best, random", value='best', key="dec split")
     dec_max_d = st.text_input("Maximum Tree Depth: integers", value='None', key="dec max depth")
     dec_rand = st.text_input("Random State: None or a single integer", value='None', key="dec random state")
 
-
-
 if model_box == 'decision tree':
-    if st.button("Add Model to Classifier Queue"):    
+    if st.button("Add Dec Tree to Classifier Queue"):    
         criterion_list = parse_text_entry(dec_crit,'string')
         splitter_list = parse_text_entry(dec_split,'string')
         max_depth_list = parse_text_entry(dec_max_d,'int')
@@ -61,6 +64,31 @@ if model_box == 'decision tree':
         decision_tree = pipeBuild_DecisionTreeClassifier(criterion=criterion_list,splitter=splitter_list,max_depth=max_depth_list,random_state=random_state_list[0])
         add_to_class_queue(name,decision_tree)
 
+
+# Extra Trees
+if model_box == 'extra trees':
+    extra_ne = st.text_input("Number of Estimators: integers", value='100', key="extra_n_estimators")
+    extra_crit = st.text_input("Criterion: gini, entropy, or log_loss", value='gini', key="extra_criterion")
+    extra_max_d = st.text_input("Maximum Tree Depth: integers", value='None', key="extra_max_depth")
+    extra_rand = st.text_input("Random State: None or a single integer", value='None', key="extra_random_state")
+    extra_ms_split = st.text_input("Minimum Sample # for Node Splits: integers", value='2', key="extra_min_samples_split")
+    extra_ms_leaf = st.text_input("Minimum Sample # Required for a Leaf: integers", value='1', key="extra_min_samples_leaf")
+
+if model_box == 'extra trees':
+    if st.button("Add Extra Trees to Classifier Queue"): 
+        extra_ne_list = parse_text_entry(extra_ne,'int')   
+        extra_crit_list = parse_text_entry(extra_crit,'string')
+        extra_max_d_list = parse_text_entry(extra_max_d,'int')
+        extra_rand_list = parse_text_entry(extra_rand,'int')
+        extra_ms_split_list = parse_text_entry(extra_ms_split,'int')
+        extra_ms_leaf_list = parse_text_entry(extra_ms_leaf,'int')
+        name = "Extra Trees"
+        extra_trees = pipeBuild_ExtraTreesClassifier(n_estimators=extra_ne_list,criterion=extra_crit_list,max_depth=extra_max_d_list,min_samples_split=extra_ms_split_list,min_samples_leaf=extra_ms_leaf_list,random_state=extra_rand_list[0])
+        add_to_class_queue(name,extra_trees)
+
+########################################################
+# End Model Variable Entries
+########################################################
 
 if st.button("Show Classifier Model Queue"):
     print(st.session_state['class_name_queue'])
